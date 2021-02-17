2b7d4b3839ae240c8f942f809d9371b0a5892bfc
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2021-02-17 12:49:58 +0400

    Add files via upload

---
+++ 11.Jquery/js/script.js
@@ -0,0 +1,69 @@
+$(function(){
+    // $("#list li").click(function(){
+    //     alert($(this).text())
+    // })
+
+    // $(document).on("click","#list li",function(){
+    //     alert($(this).text())
+    // })
+
+    // $(document).on("click","#add",function(){
+    //     let li=$("<li>Test</li>")
+    //     $("#list").append(li)
+    // })
+
+    // $(document).on("click","#add",function(){
+    //     $(".rectangle").toggle(2000);
+    //     // $(".rectangle").fadeToggle("slow").css({
+    //     //     "background-color":"tomato",
+    //     //     "height":"100px"
+    //     // })
+
+    //     // $(".rectangle").slideUp("slow",function(){
+    //     //     $(this).fadeIn("slow")
+    //     // })
+    //     // $(".rectangle").slideToggle(3000);
+    // })
+
+    // $(document).on("click","#stop",function(){
+    //     $(".rectangle").stop();
+    // })
+
+    // $("h1").text("<i>Bye Jquery</i>")
+    // $("h1").html("<i>Bye Jquery</i>")
+
+    // $(".text").val("Nese yaxshi deyilem")
+    // let count=1;
+    // $(document).on("click","#add",function(){
+    //     let li=$(`<li>${count}</li>`)
+    //     // $("#list").append(li)
+    //     // $("#list").prepend(li)
+    //     // $("#list").before(li)
+    //     $("#list").after(li)
+
+
+    //     count++;
+    // })
+
+    // $(document).on("click","#remove",function(){
+    //     // $("#list").remove()
+    //     $("#list").empty()
+
+    // })
+  
+    // $(document).on("mouseenter",".rectangle",function(){
+    //     console.log($(this).outerWidth(true));
+    // })
+
+    // console.log($("#center").parentsUntil("html"))
+
+    // console.log($("#list").find("span"));
+
+    //TASK- ACCORDIONS
+    $(document).on("click",".card-header",function(){
+        $(this).next().slideToggle("slow")
+    })
+
+});
+
+
